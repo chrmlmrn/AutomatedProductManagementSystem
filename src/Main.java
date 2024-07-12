09715e0f75e77b883039919e3e8ef2c5caf17c78
Charimel Mariano <charimelmariano27@gmail.com>
2024-07-12 17:48:18 +0800

    changes in main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 import javax.swing.*;
 
-import admin.AdminMenu;
-import cashier.CashierMenu;
-import login.Login;
-
 import java.awt.*;
 
 public class Main extends JFrame {
-    public static final String LOGIN_PANEL = "Login";
-    public static final String CASHIER_MENU_PANEL = "CashierMenu";
-    public static final String ADMIN_MENU_PANEL = "AdminMenu";
     public static final String WELCOME_PANEL = "Welcome";
 
     private CardLayout cardLayout;
@@ -27,16 +20,8 @@ public class Main extends JFrame {
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
 
-        // Initialize your panels
-        Login loginPanel = new Login(this, uniqueUserId);
-        CashierMenu cashierMenuPanel = new CashierMenu(this, uniqueUserId);
-        AdminMenu adminMenu = new AdminMenu(this, uniqueUserId);
         WelcomePage welcome = new WelcomePage(this, uniqueUserId);
 
-        // Add panels to mainPanel with unique names
-        mainPanel.add(loginPanel, LOGIN_PANEL);
-        mainPanel.add(cashierMenuPanel, CASHIER_MENU_PANEL);
-        mainPanel.add(adminMenu, ADMIN_MENU_PANEL);
         mainPanel.add(welcome, WELCOME_PANEL);
 
         // Set initial panel to Login
